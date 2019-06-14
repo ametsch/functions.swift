cee45f62444393a0202349c4cb7141f0bace31e0
ametsch <22308539+ametsch@users.noreply.github.com>
2019-06-14 08:27:31 -0400

    Update functions.swift

--- functions.swift
+++ functions.swift
@@ -44,22 +44,22 @@ class functions {
         return 0.5 * base * height
     }
     func circleArea(radius: Double) -> Double{
-        return Double.pi * radius * radius
+        return Double.pi() * radius * radius
     }
     func rectArea(width: Double, length: Double) -> Double{
         return width * length
     }
     func circumfrence(radius: Double) -> Double{
-        return Double.pi * 2 * radius
+        return Double.pi() * 2 * radius
     }
     func prismVolume(baseArea: Double, height: Double) -> Double{
         return height * baseArea
     }
     func rad2deg(radians: Double) -> Double{
-        return (Double.pi / 180.0) * radians
+        return (Double.pi() / 180.0) * radians
     }
     func deg2rad(degrees: Double) -> Double{
-        return (180.0 / Double.pi) * degrees
+        return (180.0 / Double.pi()) * degrees
     }
     func sqrt(val: Double) -> Double{
         return val.squareRoot()
